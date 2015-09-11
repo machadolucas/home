cf7745d671c8cde01ea8399ef536bf97ee6f8c75
Lucas Machado <machadolucas@me.com>
2015-09-10 22:01:36 -0300

    Fixes

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'bootstrap-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/src/main/webapp/resources/css"
-sass_dir = "src/main/webapp/resources/sass"
-images_dir = "/src/main/webapp/resources/img"
-javascripts_dir = "/src/main/webapp/resources/js"
+css_dir = "/src/main/resources/static/css"
+sass_dir = "src/main/resources/static/sass"
+images_dir = "/src/main/resources/static/img"
+javascripts_dir = "/src/main/resources/static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
