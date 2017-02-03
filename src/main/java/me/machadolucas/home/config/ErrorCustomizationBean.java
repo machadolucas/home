5559ccfdc6fd645f3b611b02b549fb61cebca871
Lucas Machado <machadolucas@me.com>
2017-02-03 15:01:25 -0200

    Update dependencies

--- src/main/java/me/machadolucas/home/config/ErrorCustomizationBean.java
+++ src/main/java/me/machadolucas/home/config/ErrorCustomizationBean.java
@@ -2,14 +2,14 @@ package me.machadolucas.home.config;
 
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ErrorCustomizationBean implements EmbeddedServletContainerCustomizer {
     @Override
-    public void customize(ConfigurableEmbeddedServletContainer container) {
+    public void customize(final ConfigurableEmbeddedServletContainer container) {
         container.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/403"));
         container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
         container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500"));
